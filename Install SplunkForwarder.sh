d1dd75a3a0c441c1983255b6e829891cb14d69ee
Daniel Machin <daniel.machin@lennar.com>
2018-05-31 17:21:39 -0400

    Updates for 7.1.0
    Added handling for user-seed.conf file.
    Added wildcards in case of .tar or .tgz archive.
    Moved command to set deployment server and boot-start before start command.
    Removed unnecessary restart at the end.

--- Install SplunkForwarder.sh
+++ Install SplunkForwarder.sh
@@ -1,29 +1,45 @@
 #!/bin/bash
 #Install or Upgrade Splunk Forwarder
 
-##FOLDER LOCATIONS
+##------------VARIABLES-----------
+##FILE/FOLDER LOCATIONS
 DOWNLOAD="/tmp"
+LIMITS="$DOWNLOAD/limits.conf"
+USERSEED="$DOWNLOAD/user-seed.conf"
 TARGET="/Applications"
-SPLUNK="$TARGET/splunkforwarder/bin/splunk"
+APPROOT="$TARGET/splunkforwarder"
+SPLUNK="$APPROOT/bin/splunk"
 LOG_NAME="COMPANY:SplunkForwarder"
+
+##CONFIG DETAILS
+SERV_IP="SERVERADDRESS:PORT"
+AUTH="USER:PASS"
+##---------------------------------
 
 logger -t $LOG_NAME "Starting Splunk Install"
 
 if [ -x "$SPLUNK" ]; then 
 	logger -t $LOG_NAME "Stopping Splunk"
-	$SPLUNK stop > /dev/null 2>&1; 
+	$SPLUNK stop > /dev/null 2>&1
+	rm -r "$APPROOT"
 fi
 
+## Unpack to Applications folder
 logger -t $LOG_NAME "Unpacking to $TARGET"
-tar -xzf "$DOWNLOAD/"splunkforwarder*.tgz -C $TARGET/
-chflags hidden /Applications/splunkForwarder
+tar -xzf "$DOWNLOAD/"splunkforwarder*.t[ag][rz] -C $TARGET/
+# Move config file
+mv $LIMITS "$APPROOT/etc/system/local/"
+mv $USERSEED "$APPROOT/etc/system/local/"
+# Hide application folder
+chflags hidden $APPROOT
 
-logger -t $LOG_NAME "Staring service"
-$SPLUNK start --accept-license --answer-yes > /dev/null 2>&1
-$SPLUNK enable boot-start > /dev/null 2>&1
+#Add server settings
 logger -t $LOG_NAME "Setting server"
-$SPLUNK set deploy-poll SERVERADDRESS:PORT -auth USER:PASS > /dev/null 2>&1
-logger -t $LOG_NAME "Restaring service"
-$SPLUNK restart > /dev/null 2>&1
+$SPLUNK set deploy-poll "$SERV_IP" -auth "$AUTH" --accept-license --answer-yes --auto-ports --no-prompt > /dev/null 2>&1 
+
+#Start Service
+logger -t $LOG_NAME "Staring service"
+$SPLUNK enable boot-start > /dev/null 2>&1
+$SPLUNK start --accept-license --answer-yes --no-prompt > /dev/null 2>&1
 
 logger -t $LOG_NAME "Install complete"
